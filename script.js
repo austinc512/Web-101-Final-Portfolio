40d3641f0a849c0d29f2d6c8a422f1c46d824c17
austinc512 <85425656+austinc512@users.noreply.github.com>
2023-06-28 16:14:44 -0500

    fixing script

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
 "use strict";
 
-navigator.clipboard
-  .readText()
-  .then(
-    (clipText) => (document.querySelector(".clip").innerText += ` ${clipText}`)
-  );
+// navigator.clipboard
+//   .readText()
+//   .then(
+//     (clipText) => (document.querySelector(".clip").innerText += ` ${clipText}`)
+//   );
+
+// const inFocus = document.getElementById("html");
+// console.log(inFocus);
+// // console.log(document.getElementsByTagName("html"));
+// inFocus.addEventListener("focus", function () {
+//   console.log(`document is in focus`);
+// });
+
+navigator.permissions.query({ name: "clipboard-read" }).then((result) => {
+  if (result.state === "granted" || result.state === "prompt") {
+    navigator.clipboard.readText().then((text) => {
+      document.querySelector(".clip").innerText += ` ${text}`;
+    });
+  }
+});
+
+// console.log(navigator.clipboard);
+
+// window.addEventListener("focus", function (event) {
+//   console.log(
+//     document.querySelector(".clip").innerText,
+//     typeof document.querySelector(".clip").innerText
+//   );
+//   // Code to run when the document comes into focus
+//   console.log("Document has come into focus");
+//   if (document.querySelector(".clip").innerText.length === 0) {
+//     navigator.clipboard
+//       .readText()
+//       .then(
+//         (clipText) =>
+//           (document.querySelector(".clip").innerText += ` ${clipText}`)
+//       );
+//   }
+// });
+
+// Uncaught (in promise) DOMException: Document is not focused.
+// how to make script run ONLY when document in focus?
 
 // document.getElementsByClassName("fullscreen").addEventListener("click", () => {
 //   toggleFullScreen();
